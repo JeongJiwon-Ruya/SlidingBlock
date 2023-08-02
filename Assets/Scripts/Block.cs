db7f3431b0cab958637dffd11a49a3eba6bcc6d2
JeongJiwon <freud.ioru@gmail.com>
2023-08-03 00:33:41 +0900

    주석 정리. 사용하지 않는 콜라이더 제어클래스 삭제

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -1,17 +1,13 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
+using System;	
+using System.Diagnostics.CodeAnalysis;
 using UnityEngine;
 using UniRx;
 using UniRx.Triggers;
 
+[SuppressMessage("ReSharper", "IdentifierTypo")]
 public class Block : MonoBehaviour
 {
-    /* 
-    block의 이동 범위 : -4 ~ +5
-     */
     private int blockSize;
-    [SerializeField]private Rigidbody2D r2D;
     private float startX;
     private float polationX;
     private float freezeY;
@@ -19,18 +15,21 @@ public class Block : MonoBehaviour
 
     private bool freezeX;
 
-    public float limit_L, limit_R;
+    public float limitL;
+    public float limitR;
+    public bool freezeL;
+    public bool freezeR;
+    private float freezeXPos;
     
-    private static Subject<InitialDrag> startDrag = new Subject<InitialDrag>();
-    public static IObservable<InitialDrag> StartDragEvent => startDrag;
-    private static ReactiveProperty<Vector3> onDrag = new ReactiveProperty<Vector3>();
-    public static IReadOnlyReactiveProperty<Vector3> OnDragEvent => onDrag;
-    public static Subject<Unit> endDrag = new Subject<Unit>();
-    public static IObservable<Unit> EndDragEvent => endDrag;
+    private static readonly Subject<InitialDrag> StartDrag = new ();
+    public static IObservable<InitialDrag> StartDragEvent => StartDrag;
+    private static readonly ReactiveProperty<Vector3> OnDrag = new ();
+    public static IReadOnlyReactiveProperty<Vector3> OnDragEvent => OnDrag;
+    private static readonly Subject<Unit> EndDrag = new ();
+    public static IObservable<Unit> EndDragEvent => EndDrag;
     
     private void Start() {
-        r2D = GetComponent<Rigidbody2D>();
-        blockSize = (int)spr.size.x;
+	    blockSize = (int)spr.size.x;
         this.OnCollisionStay2DAsObservable()
             .Where(x => x.gameObject.CompareTag("Wall"))
             .Subscribe(_ => Debug.Log("collision"));
@@ -38,142 +37,73 @@ public class Block : MonoBehaviour
 
     public Vector3 startPos;
     private void OnMouseDown() {
-        startX = this.transform.position.x;
-        startPos = this.transform.position;
+	    var position = this.transform.position;
+        startX = position.x;
+        startPos = position;
         //x값에 한해서, block의 pivot값과 마우스 클릭한 지점간의 차이값을 구해서 보정해줘야함.
-        //r2D.constraints = RigidbodyConstraints2D.FreezeRotation | RigidbodyConstraints2D.FreezePositionY;
-        freezeY = this.transform.position.y;
-        polationX = this.transform.position.x 
-            - Camera.main.ScreenToWorldPoint(Input.mousePosition).x;
+        freezeY = position.y;
+        polationX = position.x - Camera.main!.ScreenToWorldPoint(Input.mousePosition).x;
         SendStartState();
         SendPosition();
     }
 
     private void OnMouseDrag() {
-        /*
-         * 1. 왼쪽, 오른쪽 이동을 감지해야함. V
-         * 2. 현재 한쪽이라도 블록되있는지 감지해야함.
-         */
-        var temp = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        
-        //if 좌우 경계를 넘어가면 좌우 경계값으로 이동.하도록!!!!
-        if(temp.x + polationX < limit_L) {
-	        transform.position = new Vector3(limit_L, startPos.y, -9);
+	    var temp = Camera.main!.ScreenToWorldPoint(Input.mousePosition);
+	    var tempBlockX = transform.position.x;
+	    
+	    if(temp.x + polationX < limitL) {
+	        transform.position = new Vector3(limitL, startPos.y, -9);
 	        return;
-        }
-        if(limit_R < temp.x + polationX) {
-	        transform.position = new Vector3(limit_R, startPos.y, -9);
-	        return;
-        }
+	    }
+	    if(limitR < temp.x + polationX) {
+		    transform.position = new Vector3(limitR, startPos.y, -9);
+		    return;
+	    }
         
-        /*if (temp.x + polationX < startX) { //우측
-            if (!blocked_L) {
-                out_L = false;
-                transform.position = temp
-                    .GetTransformByFreezeYAndZeroZ(polationX, freezeY);
-            }
-            else {
-                if (newStandardX < temp.x) {
-                    blocked_L = false;
-                }
-            }
-        }
-        else { //좌측
-            if (!blocked_R) {
-                out_R = false;
-                transform.position = temp
-                    .GetTransformByFreezeYAndZeroZ(polationX, freezeY);
-            }
-            else {
-                if (newStandardX > temp.x) {
-                    blocked_R = false;
-                }
-            }
-        }
-
-        pastX = transform.position.x;*/
-        var tempBlockX = transform.position.x;
-
-        /*if (tempBlockX == limit_L || tempBlockX == limit_R) {
-	        
-        }
+	    
+	    if (freezeL) {
+		    if (freezeXPos < temp.x + polationX) {
+			    transform.position = temp.GetTransformByFreezeYAndZeroZ(polationX, freezeY);
+			    freezeL = false;
+			    SendPosition();
+			    return;
+		    }
+	    }
+	    if (freezeR) {
+		    Debug.Log(temp.x);
+		    if (temp.x + polationX < freezeXPos) {
+			    transform.position = temp.GetTransformByFreezeYAndZeroZ(polationX, freezeY);
+			    freezeR = false;
+			    SendPosition();
+			    return;
+		    }
+	    }
         
-        if (temp.x + polationX < pastPos.x) { //시작점 기준 왼쪽으로 이동
-	        if(limit_L <= tempBlockX || Math.Abs(tempBlockX - limit_R) < 0.01f){
-		        transform.position = temp.GetTransformByFreezeYAndZeroZ(polationX, freezeY);
-		        SendPosition();
-		        freezeX = false;
-	        } else freezeX = true;
-	        Debug.Log("A");
-        }
-        else { //시작점 기준 오른쪽으로 이동
-	        Debug.Log("B");
-	        if(Math.Abs(tempBlockX - limit_L) < 0.01f || tempBlockX <= limit_R){
-		        transform.position = temp.GetTransformByFreezeYAndZeroZ(polationX, freezeY);
-		        freezeX = false;
-		        SendPosition();
-	        }
-	        else freezeX = true;
-        }*/
-
-        if (freeze_L_X) {
-	        Debug.Log("???");
-	        if (freezeXPos < temp.x + polationX) {
-		        transform.position = temp.GetTransformByFreezeYAndZeroZ(polationX, freezeY);
-		        freeze_L_X = false;
-		        SendPosition();
-		        return;
-	        }/*
-	        else {
-		        //transform.position = new Vector3(freezeXPos, 0, 0).GetTransformByFreezeYAndZeroZ(polationX, freezeY);
-		        //transform.position = new Vector3(freezeXPos, startPos.y, -9);
-		        transform.position = new Vector3(limit_L, startPos.y, -9);
-	        }*/
-        }
-        if (freeze_R_X) {
-	        Debug.Log(temp.x);
-	        if (temp.x + polationX < freezeXPos) {
-		        transform.position = temp.GetTransformByFreezeYAndZeroZ(polationX, freezeY);
-		        freeze_R_X = false;
-		        SendPosition();
-		        return;
-	        }/*
-	        else {
-		        //transform.position = new Vector3(freezeXPos, 0, 0).GetTransformByFreezeYAndZeroZ(polationX, freezeY);
-		        //transform.position = new Vector3(freezeXPos, startPos.y, -9);
-		        transform.position = new Vector3(limit_R, startPos.y, -9);
-	        }*/
-        }
-        
-        if (limit_L <= tempBlockX && tempBlockX <= limit_R) {
-	        transform.position = temp.GetTransformByFreezeYAndZeroZ(polationX, freezeY);
-	        SendPosition();
-        }
-        
-        //Debug.Log(tempBlockX);
-        //너무 확땡겨서 초과하는 경우만 수정하면 될듯.
-        if (tempBlockX < limit_L) {
-	        freeze_L_X = true;
-	        freezeXPos = transform.position.x;
-        }
-        if (limit_R < tempBlockX) {
-	        freeze_R_X = true;
-	        freezeXPos = transform.position.x;
-        }
+	    if (limitL <= tempBlockX && tempBlockX <= limitR) {
+		    transform.position = temp.GetTransformByFreezeYAndZeroZ(polationX, freezeY);
+		    SendPosition();
+	    }
+	    
+	    if (tempBlockX < limitL) {
+		    freezeL = true;
+		    freezeXPos = transform.position.x;
+	    }
+	    if (limitR < tempBlockX) {
+		    freezeR = true;
+		    freezeXPos = transform.position.x;
+	    }
     }
 
-    public bool freeze_L_X;
-    public bool freeze_R_X;
-    private float freezeXPos;
+    [SuppressMessage("ReSharper", "Unity.InefficientPropertyAccess")]
     private void OnMouseUp() {
         var tempX = this.transform.position.x;
         if(Mathf.Abs(startX - tempX) < 0.5f) {
-            this.transform.position = transform.position.ChangeOnlyX(startX);
+            transform.position = transform.position.ChangeOnlyX(startX);
         } 
         else {
             if(blockSize % 2 == 1) {
                 //even
-                this.transform.position = transform.position.ChangeOnlyX(Mathf.Round(tempX));
+                transform.position = transform.position.ChangeOnlyX(Mathf.Round(tempX));
             } else { 
                 //odd
                 /* 
@@ -182,69 +112,31 @@ public class Block : MonoBehaviour
                 R to L : -0.5
                 */
                 if(tempX < startX)  // R to L
-                    this.transform.position = transform.position.ChangeOnlyX(Mathf.Round(tempX+0.5f) - 0.5f);
+                    transform.position = transform.position.ChangeOnlyX(Mathf.Round(tempX+0.5f) - 0.5f);
                 else
-                    this.transform.position = transform.position.ChangeOnlyX(Mathf.Round(tempX-0.5f) + 0.5f);
+                    transform.position = transform.position.ChangeOnlyX(Mathf.Round(tempX-0.5f) + 0.5f);
             }
         }
 
         SendEndState();
     }
-
-    public bool out_L, out_R;
-    public float border_L = -99, border_R = 99;
-    public float newStandardX;
     
-    public void ChangeBlockedState(GameObject border, bool isLeft, bool value) {
-        var width = border.GetComponent<SpriteRenderer>().size.x;
-        if (isLeft) {
-            //blocked_L = value;
-            if (value) {
-                int borderX = Mathf.RoundToInt(border.GetComponent<Transform>().position.x);
-                border_L = width switch {
-                    1 => borderX + 1,
-                    2 or 3 => borderX + 2,
-                    4 or 5 => borderX + 3,
-                    _ => border_L
-                };
-                //newStandardX = Camera.main.ScreenToWorldPoint(Input.mousePosition).x;
-                newStandardX = border_L;
-            }
-            else border_L = -99;
-        }
-        else {
-            //blocked_R = value;
-            if (value) {
-                int borderX = Mathf.CeilToInt(border.GetComponent<Transform>().position.x);
-                border_R = width switch {
-                    1 => borderX - 1,
-                    2 or 3 => borderX - 2,
-                    4 or 5 => borderX - 3,
-                    _ => border_R
-                };
-                //newStandardX = Camera.main.ScreenToWorldPoint(Input.mousePosition).x;
-                newStandardX = border_R;
-            }
-            else border_R = 99;
-        }
-    }
-
     public void SetDragLimit((int left, int right) limit) {
-	    limit_L = startX - limit.left;
-	    limit_R = startX + limit.right;
-	    Debug.Log($"{startX} , {limit_L}, {limit_R}");
+	    limitL = startX - limit.left;
+	    limitR = startX + limit.right;
+	    Debug.Log($"{startX} , {limitL}, {limitR}");
     }
     
     private void SendStartState() {
         Debug.Log("send");
-        startDrag.OnNext(new InitialDrag(this, spr, transform.position, blockSize));
+        StartDrag.OnNext(new InitialDrag(this, spr, transform.position, blockSize));
     }
     
     private void SendPosition() {
-        onDrag.Value = this.transform.position;
+        OnDrag.Value = this.transform.position;
     }
 
     private void SendEndState() {
-        endDrag.OnNext(Unit.Default);
+        EndDrag.OnNext(Unit.Default);
     }
 }
